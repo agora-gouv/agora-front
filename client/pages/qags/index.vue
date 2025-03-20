ea67f7ff1a737d5da7257b96fb69af68fd1f4086
ATMA <pheno62@gmail.com>
2025-03-20 18:29:11 +0100

    ajout qags

--- client/pages/qags/index.vue
+++ client/pages/qags/index.vue
@@ -12,21 +12,18 @@ const qags = (await (new QagApi().getQags(1))).value.responses
 <template>
   <div class="fr-mb-2w fr-mt-6w">
     <div class="fr-grid-row fr-mb-1w">
-      <div class="fr-col-md-6 fr-col-md" v-for="qag in qags" :key="qag.qagId">
-        <div class="fr-card fr-enlarge-link fr-mx-2w">
+      <div class="fr-col-md-6 fr-col-md fr-my-1w" v-for="qag in qags" :key="qag.qagId">
+        <div class="fr-card fr-mx-1w">
           <div class="fr-card__body">
             <div class="fr-card__content">
               <h3 class="fr-card__title">
-                <a :href="`qags/${qag.qagId}`">{{qag.title}}</a>
+                <a :href="`qags/${qag.qagId}`">{{ qag.title }}</a>
               </h3>
+              <div class="fr-card__start">
+                <img :src="qag.authorPortraitUrl" alt="Portrait de l'auteur" class="fr-card__portrait">
+                <span class="card_author">{{qag.author}}</span>
+              </div>
             </div>
-          </div>
-          <div class="fr-card__header">
-            <ul class="fr-badges-group">
-              <li>
-                <p class="fr-badge fr-badge--green-emeraude">{{ qag.thematique.picto }} {{ qag.thematique.label }}</p>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
@@ -40,5 +37,21 @@ const qags = (await (new QagApi().getQags(1))).value.responses
   line-height: 1.5;
   margin: 0;
   color: #333;
+}
+
+.card_author {
+  color: var(--text-mention-grey);
+  font-size: 0.80rem;
+  line-height: 1.25rem;
+  margin-bottom: 0;
+  vertical-align: super;
+}
+
+.fr-card__portrait {
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  margin-right: 1rem;
+  display: inline;
 }
 </style>
