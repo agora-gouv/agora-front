705eada286e9c5093f5b87f427d09a8af5474ceb
ATMA <pheno62@gmail.com>
2025-04-10 18:10:21 +0200

    feat : questionnaire données socio après formulaire

--- client/components/consultation/consultationOther.vue
+++ client/components/consultation/consultationOther.vue
@@ -14,106 +14,107 @@ const estEnCours = props.consultation.consultationDates?.endDate
 
 <template>
   <!--  <ConsultationShare :shareText="props.consultation.shareText" :shareTitle="props.consultation.title"/>-->
-  <div class="consultation fr-mt-4w">
-
-    <!-- Volet droit -->
-    <div>
-      <div v-if="userHasAnswered" class="fr-alert fr-alert--success">
-        <h6 class="fr-alert__title">Vos réponses ont bien été envoyées.</h6>
-      </div>
+  <div class="fr-mt-4w">
+    <div v-if="userHasAnswered" class="fr-alert fr-alert--success fr-mb-2w">
+      <h6 class="fr-alert__title">Vos réponses ont bien été envoyées.</h6>
+    </div>
+    <div class="consultation">
+      <!-- Volet droit -->
       <div>
-        <DsfrBadge label="Consultation en cours" no-icon/>
-      </div>
-      <div>
-        <DsfrTag :label="`${consultation.thematique.picto} ${consultation.thematique.label}`"/>
-      </div>
+        <div>
+          <DsfrBadge label="Consultation en cours" no-icon/>
+        </div>
+        <div>
+          <DsfrTag :label="`${consultation.thematique.picto} ${consultation.thematique.label}`"/>
+        </div>
 
-      <h1>{{ consultation.title }}</h1>
+        <h1>{{ consultation.title }}</h1>
 
-      <div v-if="consultation.questionsInfo" class="info-question fr-px-2w fr-py-1w">
-        <div class="fr-mb-2w">
-          <VIcon icon="ri:calendar-2-line" :inline="true" :ssr="true"/>
-          Jusqu'au
-          <Date :date="consultation.questionsInfo.endDate"/>
-        </div>
-        <div class="fr-mb-2w">
-          <VIcon :ssr="true" icon="ri:questionnaire-line" :inline="true"/>
-          {{ consultation.questionsInfo.questionCount }}
-        </div>
-        <div class="fr-mb-2w">
-          <VIcon :ssr="true" icon="ri:timer-line" :inline="true"/>
-          {{ consultation.questionsInfo.estimatedTime }}
-        </div>
-        <div class="fr-mb-2w">
-          <VIcon :ssr="true" icon="ri:group-line" :inline="true"/>
-          <span class="fr-pl-1v" v-if="consultation.questionsInfo.participantCount == 0">Aucun participant</span>
-          <span class="fr-pl-1v" v-else-if="consultation.questionsInfo.participantCount == 1">1 participant</span>
-          <span class="fr-pl-1v" v-else>{{ consultation.questionsInfo.participantCount }} participants</span>
-          <div class="fr-mt-1w fr-ml-3w" v-if="estEnCours">
-            <meter id="objectif" min="0" :max="consultation.questionsInfo.participantCountGoal"
-                   :value="consultation.questionsInfo.participantCount">
-              {{ consultation.questionsInfo.participantCount }} sur un objectif de {{ consultation.questionsInfo.participantCountGoal }}
-            </meter>
-            Prochain objectif : {{ consultation.questionsInfo.participantCountGoal }} participants !
+        <div v-if="consultation.questionsInfo" class="info-question fr-px-2w fr-py-1w">
+          <div class="fr-mb-2w">
+            <VIcon icon="ri:calendar-2-line" :inline="true" :ssr="true"/>
+            Jusqu'au
+            <Date :date="consultation.questionsInfo.endDate"/>
+          </div>
+          <div class="fr-mb-2w">
+            <VIcon :ssr="true" icon="ri:questionnaire-line" :inline="true"/>
+            {{ consultation.questionsInfo.questionCount }}
+          </div>
+          <div class="fr-mb-2w">
+            <VIcon :ssr="true" icon="ri:timer-line" :inline="true"/>
+            {{ consultation.questionsInfo.estimatedTime }}
+          </div>
+          <div class="fr-mb-2w">
+            <VIcon :ssr="true" icon="ri:group-line" :inline="true"/>
+            <span class="fr-pl-1v" v-if="consultation.questionsInfo.participantCount == 0">Aucun participant</span>
+            <span class="fr-pl-1v" v-else-if="consultation.questionsInfo.participantCount == 1">1 participant</span>
+            <span class="fr-pl-1v" v-else>{{ consultation.questionsInfo.participantCount }} participants</span>
+            <div class="fr-mt-1w fr-ml-3w" v-if="estEnCours">
+              <meter id="objectif" min="0" :max="consultation.questionsInfo.participantCountGoal"
+                     :value="consultation.questionsInfo.participantCount">
+                {{ consultation.questionsInfo.participantCount }} sur un objectif de {{ consultation.questionsInfo.participantCountGoal }}
+              </meter>
+              Prochain objectif : {{ consultation.questionsInfo.participantCountGoal }} participants !
+            </div>
           </div>
         </div>
+        <ConsultationSections :sections="consultation.body.headerSections"/>
+        <ConsultationSections :sections="consultation.body.sections"/>
+        <NuxtLink :to="'/consultations/' + consultation.id + '/questions'" v-if="estEnCours"
+                  class="fr-mb-4w fr-btn">
+          Répondre à la consultation
+        </NuxtLink>
       </div>
-      <ConsultationSections :sections="consultation.body.headerSections"/>
-      <ConsultationSections :sections="consultation.body.sections"/>
-      <NuxtLink :to="'/consultations/' + consultation.id + '/questions'" v-if="estEnCours"
-                class="fr-mb-4w fr-btn">
-        Répondre à la consultation
-      </NuxtLink>
-    </div>
 
-    <!-- Volet gauche -->
-    <div class="left-column">
-      <img class="fr-responsive-img" :src="consultation.coverUrl" alt="">
-      <div class="history" v-if="consultation.history">
-        <h2>Suivi de la consultation</h2>
-        <ul>
-          <li class="fr-grid-row" :aria-current="consultation.updateId == update.updateId" v-for="update in consultation.history">
-            <div class="fr-col-1">
-              <VIcon v-if="update.status ==='incoming'" icon="ri:checkbox-circle" :ssr="true"/>
-              <VIcon v-else icon="ri:checkbox-circle-fill" :ssr="true"/>
-            </div>
-            <div class="fr-col-11">
-              <div class="fr-ml-3w">
-                <NuxtLink :to="`/consultations/${consultation.slug}/updates/${update.slug ?? update.updateId}`"
-                          class="fr-mt-1w action">{{ update.title }}
-                </NuxtLink>
+      <!-- Volet gauche -->
+      <div class="left-column">
+        <img class="fr-responsive-img" :src="consultation.coverUrl" alt="">
+        <div class="history" v-if="consultation.history">
+          <h2>Suivi de la consultation</h2>
+          <ul>
+            <li class="fr-grid-row" :aria-current="consultation.updateId == update.updateId" v-for="update in consultation.history">
+              <div class="fr-col-1">
+                <VIcon v-if="update.status ==='incoming'" icon="ri:checkbox-circle" :ssr="true"/>
+                <VIcon v-else icon="ri:checkbox-circle-fill" :ssr="true"/>
               </div>
-              <Date class="fr-ml-3w" :date="update.date!!"/>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div v-if="consultation.goals" class="goals fr-p-3w fr-my-4w">
-        <p class="fr-h5">La consultation en un clin d'œil</p>
-        <ul class="fr-mt-2w" v-for="goal in consultation.goals">
-          <li class="fr-grid-row">
-            <span aria-hidden="true" class="picto fr-col-2 fr-pt-1w">{{ goal.picto }}</span>
-            <span class="fr-col-10" v-html="goal.description"></span>
-          </li>
-        </ul>
+              <div class="fr-col-11">
+                <div class="fr-ml-3w">
+                  <NuxtLink :to="`/consultations/${consultation.slug}/updates/${update.slug ?? update.updateId}`"
+                            class="fr-mt-1w action">{{ update.title }}
+                  </NuxtLink>
+                </div>
+                <Date class="fr-ml-3w" :date="update.date!!"/>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div v-if="consultation.goals" class="goals fr-p-3w fr-my-4w">
+          <p class="fr-h5">La consultation en un clin d'œil</p>
+          <ul class="fr-mt-2w" v-for="goal in consultation.goals">
+            <li class="fr-grid-row">
+              <span aria-hidden="true" class="picto fr-col-2 fr-pt-1w">{{ goal.picto }}</span>
+              <span class="fr-col-10" v-html="goal.description"></span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
+
+    <DsfrTile
+      v-if="consultation.downloadAnalysisUrl"
+      title="Télécharger la synthèse complète"
+      description="Pour aller plus loin, retrouvez l'analyse détaillée de l'ensemble des réponses à cette consultation."
+      :to="consultation.downloadAnalysisUrl"
+      :download="true"
+      :img-src="svgBook"
+      class="fr-mb-4w"
+    />
+    <BandeauTelechargementAdaptatif
+      v-if="consultation.questionsInfo && new Date(consultation.questionsInfo.endDate) >= new Date()"
+      title="Pour répondre à cette consultation, rendez-vous sur l’application Agora."/>
+
+    <BandeauTelechargementAdaptatif v-else title="Téléchargez l'application pour donner votre avis."/>
   </div>
-
-  <DsfrTile
-    v-if="consultation.downloadAnalysisUrl"
-    title="Télécharger la synthèse complète"
-    description="Pour aller plus loin, retrouvez l'analyse détaillée de l'ensemble des réponses à cette consultation."
-    :to="consultation.downloadAnalysisUrl"
-    :download="true"
-    :img-src="svgBook"
-    class="fr-mb-4w"
-  />
-  <BandeauTelechargementAdaptatif
-    v-if="consultation.questionsInfo && new Date(consultation.questionsInfo.endDate) >= new Date()"
-    title="Pour répondre à cette consultation, rendez-vous sur l’application Agora."/>
-
-  <BandeauTelechargementAdaptatif v-else title="Téléchargez l'application pour donner votre avis."/>
 </template>
 
 <style>
@@ -126,7 +127,7 @@ const estEnCours = props.consultation.consultationDates?.endDate
       2fr
       [right-end];
   column-gap: 2em;
-  grid-auto-flow: dense;  
+  grid-auto-flow: dense;
 }
 
 @media screen and (max-width: 767px) {
---
+++ client/pages/consultations/[id]/questions/donnees-demographiques/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'basic',
+})
+
+const consultationId = useRoute().params.id as string;
+</script>
+
+<template>
+  <h1>Psst... nous avons quelques questions supplémentaires pour vous</h1>
+  <p>Pour analyser les résultats des consultations, cela nous aide beaucoup de connaître un peu mieux le profil des participants.</p>
+  <p>Ces informations ne permettent pas de vous identifier. Vous pourrez les retrouver, les modifier ou les supprimer directement dans l’onglet “Profil” à tout instant.</p>
+  <p>Concrètement, cela nous permet :</p>
+  <ul>
+    <li>De savoir dans quelle mesure les personnes qui répondent sont représentatives de l’ensemble de la population française (plus la représentativité est forte, plus les résultats sont solides et fiables).</li>
+    <li>De voir s’il y a des différences de réponses, par exemple entre les départements ou entre les catégories d’âge.</li>  
+  </ul>
+  <p>Promis, on ne vous demandera ces infos qu’une seule fois !</p>
+
+  <p>Pour en savoir plus, lire la <NuxtLink to="/politique-confidentialite">politique de confidentialité</NuxtLink>.</p>
+
+  <NuxtLink to="/profil" class="fr-mb-4w fr-mr-2w fr-btn">
+    Commencer
+  </NuxtLink>
+  
+  <NuxtLink :to="'/consultations/' + consultationId + '?answered=true'" class="fr-mb-4w fr-btn fr-btn--secondary">
+    Ne pas répondre
+  </NuxtLink>
+</template>
+
+<style scoped>
+h1 {
+  margin-top: 1em;
+} 
+</style>
