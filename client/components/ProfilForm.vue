554cc0eb470d4899606e28ece04165fa1b68078c
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-04-22 17:18:02 +0200

    refactor: Extrait la modale de profil voteur

--- client/components/ProfilForm.vue
+++ client/components/ProfilForm.vue
@@ -117,25 +117,7 @@ const vAutofocus = {
       l'option "Je ne sais pas".
     </p>
   </DsfrModal>
-  <DsfrModal
-    :opened="profilVoteurModalOpen"
-    title="Pouquoi me demande-t-on cela ?"
-    :actions="[{ label: 'J\'ai compris', onClick: () => profilVoteurModalOpen = false }]"
-    @close="() => profilVoteurModalOpen = false"
-  >
-    <p>
-      Ces questions nous aident à comprendre si nous arrivons à toucher tous les publics, peu importe que vous soyez pour ou contre l'action
-      du Gouvernement
-    </p>
-    <p>
-      Notre objectif est de faire en sorte que des citoyens de toutes sensibilités partisanes, de tous âges et de tous horizons puissent
-      s'intéresser et participer à la vie politique.
-    </p>
-    <p>
-      Nous espérons que cette application permettra, petit à petit, de faire reculer l'abstention et peut-être, de motiver certains
-      utilisateurs à s'engager, que ce soit sur le terrain ou en ligne.
-    </p>
-  </DsfrModal>
+  <ProfilVoteurModal :open="profilVoteurModalOpen" @close="() => profilVoteurModalOpen = false" />
   <form @submit.prevent="submit" :id="id" v-bind="$attrs">
     <DsfrSelect
       name="gender"
