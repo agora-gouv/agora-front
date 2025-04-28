284fa95261b772efa47a837fdcac2411672c6c78
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-04-28 12:18:53 +0200

    feat: Ajoute les fonctionnalités en tiles

--- client/components/home/Fonctionnalites.vue
+++ client/components/home/Fonctionnalites.vue
@@ -1,41 +1,61 @@
 <script setup lang="ts">
 
-import { AsyncData } from "nuxt/app";
+import {AsyncData} from "nuxt/app";
 import AccueilContent from "~/client/types/content/accueilContent";
-import { FetchError } from "ofetch";
+import {FetchError} from "ofetch";
 
 const runtimeConfig = useRuntimeConfig()
 
 const apiBaseUrl = runtimeConfig.public.apiBaseUrl
 const routeUrl = `${apiBaseUrl}/content/page-site-vitrine-accueil`
 
-const { data: accueilContent, error } = await useFetch(routeUrl) as AsyncData<AccueilContent, FetchError>
+const {data: accueilContent, error} = await useFetch(routeUrl) as AsyncData<AccueilContent, FetchError>
 
 if (error.value) {
-  throw createError({ statusCode: error.value!.statusCode})
+  throw createError({statusCode: error.value!.statusCode})
 }
 
-const data = [
+function extractTuileData(text: string): { title: string, description: string } {
+  const [match, title, descriptionRaw] = text.match(/^<h[1-6]>(.*)<\/h[1-6]>(.*)$/);
+  const description = descriptionRaw.replace(/<([^>]*)>/, '');
+  return {
+    title,
+    description
+  }
+}
+
+const tuiles = [
   {
-    "description": accueilContent.value.texteImage1,
-    "img": "/fonctionnalite_2.png"
+    ...extractTuileData(accueilContent.value.texteImage1),
+    "imgSrc": "/fonctionnalite_1.svg"
   },
   {
-    "description": accueilContent.value.texteImage2,
-    "img": "/fonctionnalite_1.png"
+    ...extractTuileData(accueilContent.value.texteImage2),
+    "imgSrc": "/fonctionnalite_2.svg"
   },
   {
-    "description": accueilContent.value.texteImage3,
-    "img": "/fonctionnalite_3.png"
+    ...extractTuileData(accueilContent.value.texteImage3),
+    "imgSrc": "/fonctionnalite_3.svg"
   }
 ]
 </script>
 
 <template>
-  <div class="fr-grid-row fr-grid-row fr-grid-row--gutters fr-grid-row--center">
-    <Carte
-      v-for="fonctionnalite in data"
-      :data="fonctionnalite"
-    />
+  <div class="tuiles fr-grid-row fr-grid-row--gutters fr-grid-row--center">
+    <div v-for="tuile in tuiles" class="fr-col-12 fr-col-sm-6 fr-col-md-4">
+      <DsfrTile
+        :title="tuile.title"
+        :imgSrc="tuile.imgSrc"
+        :description="tuile.description"
+        class="tuile"
+      />
+    </div>
   </div>
 </template>
+
+<style>
+.tuile img {
+  object-fit: cover;
+  width: 100%;
+}
+</style>
