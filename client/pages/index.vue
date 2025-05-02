96df77457b3d2321e95de8ce37c51a7487e2008f
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-05-02 17:42:29 +0200

    feat: Supprime le texte d'intro DITP

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -12,9 +12,5 @@ useHead({
   <HomeALaUne />
   <HomeHero class="fr-pt-4w" />
   <HomeFonctionnalites />
-  <p>
-    Ce site est produit par le centre interministériel de la participation citoyenne, au sein de la DITP.
-    <a href="https://www.modernisation.gouv.fr/associer-les-citoyens/centre-interministeriel-de-la-participation-citoyenne">Qui sommes-nous ?</a>
-  </p>
   <BandeauTelechargementAdaptatif title="Également disponible sur mobile" />
 </template>
