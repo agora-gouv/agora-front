5993a5b9c4ed18f9c73176f1c705954b65fc75bd
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-05-07 15:58:18 +0200

    feat: Ajoute la redirection vers la consultation sur les données démo

--- client/components/profil/Page.vue
+++ client/components/profil/Page.vue
@@ -45,11 +45,14 @@ const vAutofocus = {
     element.focus({ preventScroll: true, focusVisible: true, })
   }
 }
+
+const redirect = useRoute().query.redirect;
 </script>
 
 <template>
   <DsfrAlert tabindex="-1" v-autofocus v-if="status === 'success'" type="success" title="Modifications enregistrées" class="fr-mb-4w">
     Vos modifications ont été enregistrées avec succès.
+    <NuxtLink :to="redirect" v-if="redirect != null">Revenir à la consultation</NuxtLink>
   </DsfrAlert>
   <DsfrAlert tabindex="-1" v-autofocus v-if="status === 'error'" type="error" title="Un problème est survenu" class="fr-mb-4w">
     <p>
