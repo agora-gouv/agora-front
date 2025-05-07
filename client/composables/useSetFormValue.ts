d67b959edd0947e768845396e6dd4b1062d0dac2
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-05-07 15:29:07 +0200

    refactor: Supprime useSetFormValue

--- client/composables/useSetFormValue.ts
+++
@@ -1,9 +0,0 @@
-export function useSetFormValue(name: string, value: string) {
-  return function setValue(event) {
-    const form = event.currentTarget.form
-    if (!form || !form.elements[name]) return;
-
-    form.elements[name].value = value
-    form.elements[name].focus()
-  }
-}
