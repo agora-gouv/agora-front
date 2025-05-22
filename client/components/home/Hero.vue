0f8342241d2e96c1d4ec104dbc7d190e349d5eac
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-05-22 15:31:26 +0200

    feat: Nouvelles images sur la page d'accueil

--- client/components/home/Hero.vue
+++ client/components/home/Hero.vue
@@ -18,14 +18,14 @@ if (error.value) {
 
 <template>
   <div class="hero break-container fr-grid-row fr-py-1w">
-    <div class="content fr-col-12 fr-col-md-6 fr-grid-row fr-grid-row--middle">
+    <div class="content fr-col-12 fr-col-md-8 fr-grid-row fr-grid-row--middle">
       <h1>La plateforme de participation citoyenne de l'État</h1>
       <p>
         Vous avez votre mot à dire sur la politique de la France, et pas seulement au moment des élections. Découvrez sur Agora les actions
         menées pour rendre notre démocratie plus forte.
       </p>
     </div>
-    <img src="/cooperation.svg" alt="" class="fr-col-12 fr-col-md-6">
+    <img src="/hero-illustration.webp" alt="" class="fr-col-12 fr-col-md-4">
   </div>
 </template>
 
@@ -33,5 +33,9 @@ if (error.value) {
 .hero {
   background-color: var(--blue-france-975-75);
   color: var(--blue-france-sun-113-625);
+}
+
+.hero img {
+  object-fit: contain;
 }
 </style>
