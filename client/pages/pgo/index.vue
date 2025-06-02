161c7fb2206189158bce4442c0b45a2d53bd485c
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-06-02 10:33:33 +0200

    feat: Wording PGO

--- client/pages/pgo/index.vue
+++ client/pages/pgo/index.vue
@@ -11,85 +11,84 @@ useHead({
 <template>
   <h1 class="fr-mt-4w">Les actions pour renforcer la démocratie</h1>
   <p>
-    Comment faire vivre une démocratie plus ouverte, plus participative et plus proche des citoyens&nbsp;? Qu’il s’agisse du <i>Grand
-    Débat</i>, des conventions citoyennes, de consultations ou de nouveaux outils numériques comme la plateforme Agora, l’État a mis en
+    Comment faire vivre une démocratie plus ouverte, plus participative et plus proche des citoyens&nbsp;? Qu’il s’agisse du Grand
+    Débat national, des Conventions citoyennes, de consultations ou de nouveaux outils numériques comme la plateforme Agora, l’État a mis en
     place plusieurs initiatives pour mieux associer les Français aux décisions publiques ces dernières années.
   </p>
   <p>
     Aujourd’hui, cette dynamique se poursuit avec de nouveaux chantiers structurants.
   </p>
-  <h2 class="fr-mt-8w">Un nouveau plan pour un gouvernement ouvert</h2>
+  <h2 class="fr-mt-8w">Le plan d'action de la France pour un Gouvernement Ouvert</h2>
   <p>
     La France est membre du <dfn>Partenariat pour un Gouvernement Ouvert (<abbr>PGO</abbr>)</dfn> depuis <time datetime="2014">2014</time>. Ce partenariat international regroupe
-    75 pays engagés à promouvoir la transparence, la participation citoyenne et la redevabilité des pouvoirs publics.
+    77 pays et 150 gouvernement locaux engagés à promouvoir la transparence, la participation citoyenne et la responsabilité des pouvoirs publics.
   </p>
   <DsfrQuote quote="
-               La France est un fervent soutien du Partenariat pour un gouvernement ouvert. C'est le principal forum mondial pour que les gouvernements,
+               La France est un fervent soutien du Partenariat pour un Gouvernement Ouvert. C'est le principal forum mondial pour que les gouvernements,
                la société civile, les citoyens et le secteur privé se rassemblent – non pas pour dire à chacun ce qu'il doit faire mais pour partager
                les innovations, les idées et les solutions de nature à améliorer nos pays. Nous devons tous être plus ambitieux.
                Nous devons tous prendre des risques, partager, apprendre et s'inspirer mutuellement
              "
              author="Emmanuel Macron, président de la République"
-             source="Déclaration sur le Partenariat pour un gouvernement ouvert, à Paris, le 24 septembre 2020"
+             source="Déclaration sur le Partenariat pour un Gouvernement Ouvert, à Paris, le 24 septembre 2020"
              sourceUrl="https://www.vie-publique.fr/discours/276468-emmanuel-macron-24092020-partenariat-pour-un-gouvernement-ouvert"
              quoteImage="/Macron.jpg"
              class="fr-mb-4w" />
-  <p>En <time datetime="2024">2024</time>, la France a lancé son <a href="https://www.modernisation.gouv.fr/files/2024-03/Plan_Action_National_2024_2026_0.pdf">4ème plan national d’action dans le cadre du <abbr>PGO</abbr></a>.</p>
+  <p>En <time datetime="2024">2024</time>, la France a lancé son <a href="https://www.modernisation.gouv.fr/files/2024-03/Plan_Action_National_2024_2026_0.pdf">4ème plan d’action national dans le cadre du <abbr>PGO</abbr></a>.</p>
   <h3 class="fr-mt-4w fr-mb-2w">Concrètement, la France s’est engagée à réaliser 17 actions&nbsp;:</h3>
   <h4 class="fr-mt-4w fr-mb-2w">Axe 1 : Améliorer et renforcer les pratiques de participation citoyenne et d’innovation démocratique</h4>
   <ul class="fr-mt-2w fr-mb-4w">
-    <li>Former les agents publics aux méthodes participatives pour qu’ils puissent mieux associer les citoyens aux politiques publiques</li>
-    <li>Créer un espace pérenne de dialogue entre l’État et la société civile pour co-construire, suivre et évaluer les politiques d’ouverture démocratique</li>
-    <li>Accompagner les pays partenaires dans la mise en œuvre de pratiques démocratiques ouvertes et transparentes, en lien avec la politique d’aide au développement de la France</li>
-    <li>Faciliter l’engagement citoyen en créant un statut du citoyen participant, c'est-à-dire un cadre protecteur et reconnu quand on participe à une démarche participative (indemnisation, congé citoyen, valorisation des compétences, etc.)</li>
+    <li>Réaliser un état des lieux des démarches de participation et d’innovation démocratique</li>
+    <li>Former les agents publics aux méthodes participatives pour mieux associer les citoyens aux politiques qui les concernent</li>
+    <li>Créer et animer dans la durée un espace de dialogue entre l’État et la société civile pour construire, suivre et évaluer les actions de renouveau démocratique</li>
+    <li>Accompagner les pays partenaires de la politique d’aide au développement dans la mise en œuvre de nouvelles pratiques démocratiques</li>
+    <li>Étudier la création d’un statut du citoyen participant favorisant l’accès de tous les citoyens aux démarches de participation citoyenne</li>
   </ul>
-  <h4 class="fr-mt-4w fr-mb-2w">Axe 2 : Promouvoir l’inclusion et l’engagement sur tout le territoire</h4>
+  <h4 class="fr-mt-4w fr-mb-2w">Axe 2 : Renforcer l’inclusion et l’engagement sur l’ensemble du territoire</h4>
   <ul class="fr-mt-2w fr-mb-4w">
-    <li>Favoriser l’engagement des jeunes en les sensibilisant au fonctionnement démocratique à l’aide d’un outil pédagogique interactif inspiré de la <i>«&nbsp;Fresque du Climat&nbsp;»</i></li>
-    <li>Rendre l’inscription sur les listes électorales plus simple et intuitive grâce à un parcours numérique pensé autour des moments de vie clés</li>
-    <li>Répondre au mieux aux besoins spécifiques en santé des territoires en s’appuyant sur les parties prenantes</li>
-    <li>Co-construire localement des projets scolaires avec la communauté éducative pour favoriser la réussite et le bien-être des élèves</li>
-    <li>Impliquer tous les acteurs (citoyens, entreprises, collectivités) dans la transition écologique grâce à des outils pédagogiques et des démarches participatives à l’échelle locale</li>
-    <li>Développer la culture numérique et de l’<abbr>IA</abbr> à travers des actions pédagogiques, participatives et ouvertes à tous pour mieux comprendre les enjeux et usages des données</li>
-    <li>Partager les bonnes pratiques en matière de lutte contre la désinformation auprès des Français de l'étranger en diffusant des contenus pédagogiques fiables et en valorisant l’expertise des acteurs publics et associatifs</li>
+    <li>Favoriser l’engagement civique des jeunes à travers la diffusion d’outils interactifs comme les « Fresques de la démocratie »</li>
+    <li>Faciliter l’inscription sur les listes électorales grâce à un parcours numérique simplifié</li>
+    <li>Répondre aux besoins spécifiques en santé dans les territoires en s’appuyant sur des concertations territoriales</li>
+    <li>Expérimenter de nouveaux projets scolaires avec les communautés éducatives locales pour favoriser le bien-être des élèves et lutter contre les inégalités scolaires</li>
+    <li>Impliquer un maximum d’acteurs (société civile, entreprises, collectivités) dans la planification écologique du pays</li>
+    <li>Renforcer le dialogue social et l’information des citoyens pour développer une culture du numérique et de l’intelligence artificielle</li>
   </ul>
   <h4 class="fr-mt-4w fr-mb-2w">Axe 3 : Mettre le numérique au service d’une action publique plus transparente et plus participative.</h4>
   <ul class="fr-mt-2w fr-mb-4w">
     <li>Permettre à chacun de suivre les résultats concrets de l’action publique dans son territoire via un <a
       href="https://www.info.gouv.fr/politiques-prioritaires">baromètre en ligne, actualisé et accessible</a></li>
-    <li>Organiser régulièrement des consultations citoyennes nationales pour recueillir des avis sur les politiques majeures en cours de construction</li>
+    <li>Organiser régulièrement des consultations citoyennes sur les grands enjeux de politiques publiques en s’appuyant sur la plateforme Agora</li>
     <li>Soutenir les logiciels libres et les projets numériques d’intérêt général en facilitant leur adoption dans les services publics et leur collaboration avec l’État</li>
-    <li>Mettre en place un observatoire du service public de la donnée</li>
-    <li>Mettre à disposition un guichet d'accès centralisé aux données de la transition écologique</li>
+    <li>Mettre en place un observatoire du service public de la donnée en vue de faciliter leur réutilisation et leur transparence</li>
+    <li>Mettre à disposition un guichet d'accès centralisé des données en matière de transition écologique</li>
   </ul>
   <a href="https://www.modernisation.gouv.fr/files/2024-03/Plan_Action_National_2024_2026_0.pdf"
      target="_blank"
      class="fr-link"
      rel="noreferrer noopener">
-    Consulter la liste détaillée des 17 engagements du plan <abbr>PGO</abbr> <time>2024-2026</time>
+    Consulter la liste détaillée des 17 engagements du 4ème plan d'action <time>2024-2026</time>
   </a>
   
-  <h2 class="fr-pt-8w">La publication des cahiers de doléances du <i>Grand débat</i></h2>
+  <h2 class="fr-pt-8w">La publication des cahiers de doléances du Grand Débat national</h2>
   <p>
-    Dans le cadre du <i>Grand Débat national</i> en <time>2019</time>, les Français ont rempli près de 20 000 cahiers de doléances dans les
-    mairies. Pendant plusieurs années, ces cahiers étaient consultables uniquement sur dérogation. Ces documents constituent un témoignage
-    inédit de la parole citoyenne et des aspirations des Français. Ils sont désormais accessibles à tous sans dérogation dans les archives
+    Dans le cadre du Grand Débat national de <time>2019</time>, les Français ont rempli près de 20 000 cahiers de doléances dans les
+    mairies. Ces documents constituent un témoignage inédit de la parole citoyenne et des aspirations des Français. Pendant plusieurs années, ces cahiers étaient consultables uniquement sur dérogation. Ils sont désormais accessibles à tous sans dérogation dans les archives
     nationales et départementales. La prochaine étape est de les rendre consultables directement sur Internet.
   </p>
   
   <ol class="fr-mt-2w fr-mb-4w">
-    <li><b><time datetime="2025-01">Janvier 2025</time>&nbsp;:</b> annonce par le Premier ministre François Bayrou de la reprise de l’étude des cahiers de doléances dans son discours de politique générale au Parlement.</li>
-    <li><b><time datetime="2025-04-29">29 avril 2025</time>&nbsp;:</b> ouverture sans dérogation à tous les citoyens, grâce à un arrêté publié au <i>Journal officiel</i>.</li>
-    <li><b><time datetime="2025-05">Mai 2025</time>&nbsp;:</b> lancement d’un groupe de travail chargé de rendre ces doléances lisibles, exploitables et ouvertes à tous d’ici <time>2026</time>.</li>
+    <li><b><time datetime="2025-01">Janvier 2025</time>&nbsp;:</b> annonce par le Premier ministre François Bayrou de la reprise de l’étude des cahiers de doléances dans sa déclaration de politique générale au Parlement</li>
+    <li><b><time datetime="2025-04-29">29 avril 2025</time>&nbsp;:</b> ouverture sans dérogation à tous les citoyens, grâce à un arrêté publié au <i>Journal officiel</i></li>
+    <li><b><time datetime="2025-06-05">Juin 2025</time>&nbsp;:</b> lancement d’un groupe de travail chargé de rendre ces doléances lisibles, exploitables et ouvertes à tous d’ici <time>2026</time></li>
   </ol>
 
   <DsfrQuote quote="
                Pour le gouvernement, l’association des citoyennes et des citoyens à l’élaboration des politiques publiques est une question
                majeure et une condition indispensable de la solidité et de la vitalité de notre démocratie.
              "
-             author="Patrick Mignola, Ministre délégué auprès du Premier ministre, chargé des Relations avec le Parlement"
+             author="Patrick Mignola, ministre délégué auprès du Premier ministre, chargé des Relations avec le Parlement"
              quoteImage="/Mignola.jpg"
-             source="Débat à l'assemblée nationale le 11 mars 2025"
+             source="Débat à l'Assemblée nationale le 11 mars 2025"
              class="fr-mb-4w" />
 
    <div class="bandeau-cta fr-grid-row fr-grid-row--gutters fr-p-5w">
@@ -97,15 +96,15 @@ useHead({
     <p class="fr-h3 fr-col-11 fr-m-0 fr-py-0">Téléchargez l’application Agora pour être prévenu(e) dès que les cahiers de doléance sont en ligne&nbsp;!</p>
    </div>
   
-  <h2 class="fr-pt-8w">Une nouvelle convention citoyenne à l’automne</h2>
+  <h2 class="fr-pt-8w">Les Conventions citoyennes</h2>
   <p>Les conventions citoyennes sont devenues, en quelques années, un format incontournable de la démocratie participative en France.</p>
   <ul class="fr-mt-2w fr-mb-4w">
-    <li>La <b><i>Convention citoyenne pour le climat</i></b> (2019-2020) a débouché sur de nombreuses mesures intégrées à la loi <i>Climat et Résilience</i>.</li>
-    <li>Les conclusions des travaux de la <b><i>Convention sur la fin de vie</i></b> (2022-2023) sont actuellement débattues à l’Assemblée nationale.</li>
+    <li>La <a href="/fiches-inventaire/9"><b><i>Convention citoyenne pour le climat</i></b></a> (2019-2020) a débouché sur de nombreuses mesures intégrées à la loi <i>Climat et Résilience</i> de 2021</li>
+    <li>La <a href="/fiches-inventaire/10"><b><i>Convention citoyenne sur la fin de vie</i></b></a> (2022-2023) a débouché sur 2 propositions de loi adoptées par l'Assemblée nationale le <time datetime="2025-05-27">27 mai 2025</time></li>
   </ul>
   <p class="fr-mb-8w">
-    A partir de <time datetime="2025-06">juin 2025</time>, une nouvelle convention s’organise sur le thème des temps de l’enfant : rythmes
-    scolaires, temps extrascolaires, conciliation avec la vie familiale&hellip; Ce sujet, au croisement de l’éducation, du bien-être et de
+    À partir de <time datetime="2025-06">juin 2025</time>, une nouvelle convention s’organise sur le thème des temps de l’enfant : rythmes
+    scolaires, temps extrascolaires, conciliation avec la vie familiale, etc. Ce sujet, au croisement de l’éducation, du bien-être et de
     l’organisation de la société, mobilise à nouveau un panel de citoyens tirés au sort pour formuler des recommandations concrètes.
   </p>
   <BandeauTelechargementAdaptatif title="Activez les notifications sur l'application Agora pour ne rien louper." />
