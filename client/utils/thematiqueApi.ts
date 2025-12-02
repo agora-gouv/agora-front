307c1c6e9515cb3a50269162c3ede0c3d0f848fb
sandrine.belin <sandrine.belin@octo.com>
2025-12-02 10:52:45 +0100

    feat: Récupérer les thématiques depuis le CMS

---
+++ client/utils/thematiqueApi.ts
@@ -0,0 +1,27 @@
+import { ThematiqueApiDTO } from "~/client/types/thematique/thematique";
+
+export class ThematiqueApi {
+  private baseUrl = useRuntimeConfig().public.apiBaseUrl;
+
+  async getAll() {
+    const routeThematiqueResponsesUrl = `${this.baseUrl}/thematiques`
+
+    const {data: thematiques, error}
+      = await useFetch<ThematiqueApiDTO[]>(routeThematiqueResponsesUrl)
+
+    if (error.value) throw createError({statusCode: error.value.statusCode})
+
+    return thematiques.value!
+  }
+
+  async get(id: string) {
+    const routeThematiqueResponsesUrl = `${this.baseUrl}/thematiques/${id}`
+
+    const {data: thematiques, error}
+      = await useFetch<ThematiqueApiDTO>(routeThematiqueResponsesUrl)
+
+    if (error.value) throw createError({statusCode: error.value.statusCode})
+
+    return thematiques.value!
+  }
+}
