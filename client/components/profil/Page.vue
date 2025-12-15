b4ae773dd5ff77db292d00f179b9ad7bb251c330
sandrine.belin <sandrine.belin@octo.com>
2025-12-15 15:28:27 +0100

    feat: Ajouter condition redirect

--- client/components/profil/Page.vue
+++ client/components/profil/Page.vue
@@ -45,12 +45,13 @@ const vAutofocus = {
     element.focus({ preventScroll: true, focusVisible: true, })
   }
 }
+const redirect = useRoute().query.redirect;
 </script>
 
 <template>
   <DsfrAlert tabindex="-1" v-autofocus v-if="status === 'success'" type="success" title="Modifications enregistrées" class="fr-mb-4w">
     Vos modifications ont été enregistrées avec succès.
-    <NuxtLink to="/je-participe">Répondre aux autres consultations</NuxtLink>
+    <NuxtLink to="/je-participe" v-if="redirect != null">Répondre aux autres consultations</NuxtLink>
   </DsfrAlert>
   <DsfrAlert tabindex="-1" v-autofocus v-if="status === 'error'" type="error" title="Un problème est survenu" class="fr-mb-4w">
     <p>
